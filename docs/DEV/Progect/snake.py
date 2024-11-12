4d81b514c9d9fca5bd26421708b02fb29be314ae
cosmos <ZJUZJU324@outlook.com>
2024-11-12 15:15:01 +0800

    change snake

--- docs/DEV/Progect/snake.py
+++ docs/DEV/Progect/snake.py
@@ -1,11 +1,12 @@
-# 面向对象 用python写程序的思维
 import sys
 import random
 import pygame
 
-# 定义全局变量
-SCREEN_X = 600
-SCREEN_Y = 600
+# 全局定义
+SCREEN_X = 700
+SCREEN_Y = 700
+
+# 面向对象 # 用python写程序的思维
 
 # 蛇
 class Snake(object):
@@ -16,7 +17,7 @@ class Snake(object):
         self.direction = pygame.K_RIGHT
         self.body = []
 
-        for x in range(5):
+        for x in range(8):
             self.addnode()
         
     #方法：
@@ -24,20 +25,19 @@ class Snake(object):
         # 2.死亡
         # 3.移动
         # 4.方向
-
     def addnode(self):
         left, top = (0, 0)
         if self.body:
             left, top = (self.body[0].left, self.body[0].top)
-        node = pygame.Rect(left, top, 25, 25)
+        node = pygame.Rect(left, top, 15, 15)
         if self.direction == pygame.K_LEFT:
-            node.left -= 25
+            node.left -= 15
         elif self.direction == pygame.K_RIGHT:
-            node.left += 25
+            node.left += 15
         elif self.direction == pygame.K_UP:
-            node.top -= 25
+            node.top -= 15
         elif self.direction == pygame.K_DOWN:
-            node.top += 25
+            node.top += 15
         self.body.insert(0, node)
     
     # 删除
@@ -52,7 +52,7 @@ class Snake(object):
         if self.body[0].y not in range(SCREEN_Y):
             return True
         # 撞自己
-        if self.body[0] in self.body[1:]:
+        if self.body[0] in self.body[1:]:  # 序列的切片
             return True
         return False
 
@@ -75,16 +75,16 @@ class Snake(object):
 # 食物
 class Food:
     def __init__(self):
-        self.rect = pygame.Rect(-25, 0, 25, 25)
+        self.rect = pygame.Rect(-15, 0, 15, 15)
     
     def remove(self):
-        self.rect.x = -25
+        self.rect.x = -15
 
     def set(self):
-        if self.rect.x == -25:
+        if self.rect.x == -15:
             allpos = []
-            # 不能靠墙太近 25 ~ SCREEN_X - 25 之间
-            for pos in range(25, SCREEN_X - 25, 25):
+            # 不能靠墙太近 15 ~ SCREEN_X - 15 之间
+            for pos in range(15, SCREEN_X - 15, 15):
                 allpos.append(pos)
             self.rect.left = random.choice(allpos)
             self.rect.top = random.choice(allpos)
@@ -94,7 +94,7 @@ def show_text(screen, pos, text, color, font_bold = False, font_size = 60, font_
     # 获取系统字体， 设置文字大小
     cur_font = pygame.font.SysFont("宋体", font_size)
     # 设置是否加粗
-    cur_font.set_bode(font_bold)
+    cur_font.set_bold(font_bold)
     # 设置是否斜体
     cur_font.set_italic(font_italic)
     # 设置文字内容
@@ -131,14 +131,14 @@ def main():
         if not isdead:
             snake.move()
         for ret in snake.body:
-            pygame.draw.rect(screen, (20, 220, 39), rect, 0)
+            pygame.draw.rect(screen, (20, 220, 39), ret, 0)
 
             # 显示死亡文字
         isdead = snake.isdead()
 
         if isdead:
-            show_text(screen, (100, 200), 'YOU DEAD!', (227, 29, 18), False, 100)
-            show_text(screen, (150, 260), 'press space to try again...', (0, 0, 22), False, 30)
+            show_text(screen, (150, 260), 'YOU DEAD!', (227, 29, 18), False, 100)
+            show_text(screen, (180, 350), 'press space to try again...', (0, 0, 22), False, 40)
             
         # 食物处理    吃到 + 50    食物与蛇头   蛇的长度 + 1方块
         if food.rect == snake.body[0]:
@@ -150,7 +150,7 @@ def main():
         pygame.draw.rect(screen, (136, 0, 24), food.rect, 0)
 
         # 显示分数文字
-        show_text(screen, (50, 500), 'Scores : ' + str(scores), (223, 223, 223))
+        show_text(screen, (20, 20), 'Scores : ' + str(scores), (192, 192, 192))
 
         pygame.display.update()  # 更新
         clock.tick(10)
